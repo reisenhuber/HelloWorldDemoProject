8f3651488377b6696a6ed95ad68b6654aedd3f00
XMG\Roli <bcrol@hotmail.com>
2019-04-02 12:10:02 +0200

    commit 1 mit neuer demoexception

--- Program.cs
+++ Program.cs
@@ -10,8 +10,37 @@ namespace HelloWorldDemoProject
     {
         static void Main(string[] args)
         {
-            var teilnehmer = "Teilnehmer 1";
-            Console.WriteLine($"Hello {teilnehmer}");
+            //Console.WriteLine(args.Length);
+            try
+            {
+                Console.WriteLine($"Hello {args[1]}");
+                Console.WriteLine("Super, kein Fehler");
+                Console.Read();
+            }
+            //catch (IndexOutOfRangeException IOORE) //when (IOORE.Message.Contains("Index"))
+            //{
+            //    Console.WriteLine(IOORE.Message);
+            //}
+
+            catch (Exception)
+            {
+
+                //throw new DemoException("");
+            }
+            Console.Read();
+        }
+    }
+
+    public class DemoException : Exception
+    {
+        public DemoException(string message)
+        {
+
+        }
+
+        public DemoException()
+        {
+
         }
     }
 }
