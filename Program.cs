b11c541dab5e167b00d4e8487960741169cd55d8
XMG\Roli <bcrol@hotmail.com>
2019-04-02 10:50:54 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldDemoProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var teilnehmer = "Teilnehmer 1";
+            Console.WriteLine($"Hello {teilnehmer}");
+        }
+    }
+}
